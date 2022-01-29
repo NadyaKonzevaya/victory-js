c74400cc9d89ff5cbfd3feb61c2b399720f1645f
NadyaKonzevaya <borisova.nadya@gmail.com>
2022-01-29 15:50:58 +0300

    добавляет задачу 1

---
+++ js/page1.js
@@ -0,0 +1,19 @@
+// 1. Используя функцию if...else,
+//напишите код, который будет спрашивать:
+//"Какое официальное название JavaScript?"
+//Если пользователь вводит "ECMAScript",
+//то показать через alert: "Верно!"
+//в противном случае отобразить:"Не знаете? ECMAScript!"
+
+
+const userInput = prompt("Какое официальное название JavaScript?")
+let message = "Не знаете? ECMAScript!";
+
+
+
+if(userInput === "ECMAScript") {
+    message = "Верно!";
+}
+
+alert(message);
+console.log(userInput);
